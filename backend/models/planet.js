7ebc721ceec4149299f961d79dfdc76b1a9f3796
Aleksandra <baliaks1@stu.vistula.edu.pl>
2025-10-17 22:03:33 +0300

    Fix Planet model definition and sync with database

--- backend/models/planet.js
+++ backend/models/planet.js
@@ -1,17 +1,16 @@
-const { Sequelize, DataTypes } = require("sequelize");
+const { Sequelize, DataTypes } = require('sequelize');
 const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite"
+  dialect: 'sqlite',
+  storage: 'database.sqlite'
 });
 
-const Planet = sequelize.define("Planet", {
+const Planet = sequelize.define('Planet', {
   name: { type: DataTypes.STRING, allowNull: false },
   system: { type: DataTypes.STRING, allowNull: false },
   climate: { type: DataTypes.STRING, allowNull: false },
-  population: { type: DataTypes.INTEGER, allowNull: false },
-  surfaceType: { type: DataTypes.STRING, allowNull: false }
+  population: { type: DataTypes.INTEGER, allowNull: false }
 });
 
 sequelize.sync();
-
 module.exports = { Planet, sequelize };
+

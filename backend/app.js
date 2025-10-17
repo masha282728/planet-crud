3148a33bfe7bbbcda378fdf0c8968c1ea4338237
Aleksandra <baliaks1@stu.vistula.edu.pl>
2025-10-17 22:05:12 +0300

    Setup Express app and connect Planet routes

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const { sequelize } = require("./models/planet");
-const planetRoutes = require("./routes/planetRoutes");
+const express = require('express');
+const bodyParser = require('body-parser');
+const { sequelize } = require('./models/planet');
+const planetRoutes = require('./routes/planetRoutes');
 
 const app = express();
 app.use(bodyParser.json());
-app.use("/api/planet", planetRoutes);
-
-app.use(express.static(path.join(__dirname, "../frontend")));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
-});
+app.use('/api/planet', planetRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-sequelize.authenticate()
-  .then(() => {
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-  })
-  .catch(err => console.log("DB connection error", err));
+sequelize.authenticate().then(() => {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}).catch(err => console.log('DB connection error', err));

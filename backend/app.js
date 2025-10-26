6247e897b1028c201e7f87ed3e9b32576623eef2
masha282728 <masamartysko@gmail.com>
2025-10-26 12:49:51 +0100

    Update app.js to use process.env.PORT for Render deployment

--- backend/app.js
+++ backend/app.js
@@ -17,5 +17,5 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🌍 Server działa: http://localhost:${PORT}`));

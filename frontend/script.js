35ccc3bfe086a2b71786f905873b4ecb0531fb0e
masha282728 <masamartysko@gmail.com>
2025-10-29 14:56:52 +0100

    Update API endpoint for planet data

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const api = 'http://localhost:3000/api/planets';
+const api = 'http://https://planet-crud-amber.vercel.app//api/planets';
 const form = document.getElementById('planetForm');
 const table = document.querySelector('#planetTable tbody');
 

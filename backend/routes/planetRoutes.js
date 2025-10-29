0ef121ff44c9a781ee331fea537ea88fb11015dc
masha282728 <masamartysko@gmail.com>
2025-10-29 15:21:52 +0100

    Update planetRoutes.js

--- backend/routes/planetRoutes.js
+++ backend/routes/planetRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const c = require('../controllers/planetController');
 router.get('/', c.getAll);
+router.get('/api', c.getAll);
 router.get('/:id', c.getOne);
 router.post('/', c.create);
 router.put('/:id', c.update);

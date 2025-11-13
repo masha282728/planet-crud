21efef7ae0a83538729bb8df978c352a375e9539
masha282728 <masamartysko@gmail.com>
2025-11-13 14:42:26 +0100

    Refactor planetRoutes to use express.Router() directly

--- backend/routes/planetRoutes.js
+++ backend/routes/planetRoutes.js
@@ -1,9 +1,10 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const c = require('../controllers/planetController');
 router.get('/', c.getAll);
-router.get('/api', c.getAll);
 router.get('/:id', c.getOne);
 router.post('/', c.create);
 router.put('/:id', c.update);
 router.delete('/:id', c.delete);
+
 module.exports = router;
